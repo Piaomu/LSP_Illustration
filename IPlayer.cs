3f46e693452ee72c0f90105dcc5c0d30ba86b754
piaomu <driftwood.wahl@gmail.com>
2021-07-20 12:32:47 -0500

    Add project files.

---
+++ IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Illustration
+{
+    interface IPlayer
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        decimal Salary { get; set;  }
+
+        void ShouldHePlay(int fouls, int minutesPlayed);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LSP_Illustration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
